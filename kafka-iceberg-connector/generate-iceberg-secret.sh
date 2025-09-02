11a72b3b401c6a62fa5293ac1dabcc85a2f26bcb
KC Lai <kuanchoulai10@gmail.com>
2025-09-02 19:19:29 +0800

    Refactor secret generation script and update installation process for Iceberg connector

--- kafka-iceberg-connector/generate-iceberg-secret.sh
+++ kafka-iceberg-connector/generate-iceberg-secret.sh
@@ -4,11 +4,15 @@ set -e
 
 echo "Creating Kubernetes secret for Accessing Iceberg Table in AWS S3..."
 
+read -p "  Enter AWS Region: " AWS_REGION
+read -p "  Enter AWS Access Key: " AWS_ACCESS_KEY_ID
+read -p "  Enter AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
+
 kubectl create secret generic iceberg-secret \
+  --from-literal=awsRegion=$AWS_REGION \
   --from-literal=awsAccessKey=$AWS_ACCESS_KEY_ID \
   --from-literal=awsSecretAccessKey=$AWS_SECRET_ACCESS_KEY \
-  --from-literal=awsRegion=$AWS_ICEBERG_REGION \
   --namespace kafka-cdc \
   --dry-run=client -o yaml > "./iceberg-secret.yaml"
 
-echo "iceberg-secret.yaml file has been generated."
+echo "iceberg-secret.yaml created."
--- kafka-iceberg-connector/install.sh
+++ kafka-iceberg-connector/install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+bash generate-iceberg-secret.sh
+
 kubectl apply -f iceberg-secret.yaml -n kafka-cdc
 kubectl apply -f iceberg-connect-cluster.yaml -n kafka-cdc
 kubectl apply -f iceberg-connector.yaml -n kafka-cdc
