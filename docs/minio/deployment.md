8b24554612cdbd589b018078ca04ecb20e667a10
KC Lai <kuanchoulai10@gmail.com>
2025-09-26 20:44:15 +0800

    Add Thanos Receive and Store components with ServiceMonitors and ConfigMaps
    - Introduced ServiceMonitor for Thanos Receive Ingestor to monitor its health.
    - Created ConfigMap for hashring configuration used by Thanos Receive Router.
    - Added Deployment for Thanos Receive Router with necessary configurations and probes.
    - Defined Service for Thanos Receive Router to expose its endpoints.
    - Created ServiceAccount for Thanos Receive Router.
    - Added Services and StatefulSets for Thanos Store shards (0, 1, 2) with appropriate configurations.
    - Implemented ServiceAccount for Thanos Store components.
    - Added ServiceMonitor for Thanos Store to monitor its health.
    - Updated Jsonnet configuration to include all new components and their configurations.

---
+++ docs/minio/deployment.md
@@ -0,0 +1,6 @@
+# MinIO Deployment
+
+```bash
+cd ~/Projects/retail-lakehouse/minio
+bash install.sh
+```
---
+++ thanos/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+# Install Prometheus Operator using YAML files
+# https://prometheus-operator.dev/docs/getting-started/installation/#install-using-yaml-files
+PROMETHEUS_OPERATOR_NAMESPACE=prometheus-operator-system
+kubectl create namespace $PROMETHEUS_OPERATOR_NAMESPACE || true
+TMPDIR=$(mktemp -d)
+PROMETHEUS_OPERATOR_VERSION=v0.85.0
+curl -s "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/refs/tags/$PROMETHEUS_OPERATOR_VERSION/kustomization.yaml" > "$TMPDIR/kustomization.yaml"
+curl -s "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/refs/tags/$PROMETHEUS_OPERATOR_VERSION/bundle.yaml" > "$TMPDIR/bundle.yaml"
+(cd $TMPDIR && kustomize edit set namespace $PROMETHEUS_OPERATOR_NAMESPACE) && kubectl create -k "$TMPDIR"
+sleep 5
+kubectl wait --for=condition=Ready pods -l app.kubernetes.io/name=prometheus-operator -n $PROMETHEUS_OPERATOR_NAMESPACE --timeout=180s
+
+
+# Install Cert-Manager using YAML files
+CERT_MANAGER_VERSION=v1.18.2
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/$CERT_MANAGER_VERSION/cert-manager.yaml
+
+
+# Install OpenTelemetry Operator using YAML files
+OTEL_OPERATOR_VERSION=v0.132.0
+kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/download/$OTEL_OPERATOR_VERSION/opentelemetry-operator.yaml
+
+
+cd ~/Projects/retail-lakehouse/thanos
+kubectl create namespace thanos || true
+# Install Jaeger using Otel Operator
+kubectl apply -f manifests/jaeger-thanos.yaml -n thanos
+# Install Thanos using kube-thanos jsonnet
+# https://github.com/thanos-io/kube-thanos
+jb install github.com/thanos-io/kube-thanos/jsonnet/kube-thanos@main
+rm -f manifests/thanos-*
+jsonnet -J vendor -m manifests/ thanos.jsonnet | xargs -I{} sh -c "cat {} | yq -P > {}.yaml; rm -f {}" -- {}
+# Install the rest of components
+kubectl apply -f manifests/
